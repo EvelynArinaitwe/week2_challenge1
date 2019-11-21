691c1661c6aec2eb8a2330eca8050c6fcbf34145
EvelynArinaitwe <eatumusiime@gmail.com>
2019-11-21 08:54:49 +0100

    add existing file

--- object.js
+++ object.js
@@ -3,21 +3,12 @@ function square(x){
     return x*x;
 }
 
-let obj = {
-    2 : square(2),
-    3 : square(3),
-    4 : square(4),
-    5 : square(5),
-    6 : square(6),
-    7 : square(7),
-    8 : square(8),
-    9 : square(9),
-    10: square(10),
-    11: square(11),
-    12: square(12),
-    13: square(13),
-    14: square(14),
 
-};
 
-console.log("The object is: ", obj);
+    let f= {};
+    for(let i =2;i<15;i++){
+        f[i] = square(i);
+       // console.log(f);
+    }
+
+    console.log("obect is: ",f);
